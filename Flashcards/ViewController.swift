87fa1ff72874a4a4d6bc567269c8c406225a3324
BriYon Gladden <bjgladden@aggies.ncat.edu>
2018-10-13 16:42:06 -0400

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Jadi Gladden on 10/13/18.
+//  Copyright © 2018 Swervo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
